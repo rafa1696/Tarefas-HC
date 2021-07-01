ca05bf84fb5ef6bb29acfc9f2ac50782395afbdc
rafa1696 <79325328+rafa1696@users.noreply.github.com>
2021-07-01 13:08:34 -0300

    Iteração 0.9, com função de adicionar tarefa na lista

--- js/js.js
+++ js/js.js
@@ -4,9 +4,11 @@ const newListInput = document.querySelector("[data-new-list-input]");
 
 let lists = [];
 
+// Função para monitorar o botão enviar do formulário
+
 newListForm.addEventListener("submit", function(e) {
     e.preventDefault();
-    const listName = newListInput.nodeValue;
+    const listName = newListInput.value;
     if (listName === null || listName === "") return;
 
     const list = createList(listName);
@@ -15,11 +17,37 @@ newListForm.addEventListener("submit", function(e) {
     render();
 })
 
+// Função para remover elemento com botão "-"
+
+
+
+
+// Função para criar um objeto com ID baseado na hora/data da criação e nome inserido
+
+function createList(name) {
+    return {
+        id: Date.now().toString(),
+        name: name
+    }
+}
+
+// Função para renderizar cada elemento adicionado à lista 
+
 function render() {
+    clearElement(listContainer)
     lists.forEach(function(list){
         const item = document.createElement("li");
         item.classList.add("item");
-        item.innerText = list;
+        item.innerText = list.name;
         listContainer.appendChild(item);
     })
 }
+
+function clearElement(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+
+render();
